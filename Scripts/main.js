640e29a1a2a141e2576e64ba8be1359220bf62f8
Ginyto <agianlucca@gmail.com>
2022-02-10 17:14:47 +0100

    caroussel almost done

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,11 +1,40 @@
 const jarvis = {
 
+    pathfinder(param) {
+        if (param == 0) {
+            document.location.pathname = "/Pages/Landing.html";
+        }
+
+        if (param == 1) {
+            document.location.pathname = "/Pages/Flashcards.html";
+        }
+
+        if (param == 2) {
+                document.location.pathname = "/Pages/Addcarte.html";
+        }
+        
+        if (param == 3) {
+            document.location.pathname = "/Pages/Gestionnaire.html";
+        }
+
+        if (param == 4) {
+                document.location.pathname = "/Pages/Pomodoro.html";
+        }
+
+        if (param == 5) {
+                document.location.pathname = "/Pages/About.html";
+            }
+
+    },
+
+
     image_scroll() {
         
     },
 
     test() {
         alert("test validé")
+        console.log(document.getElementById(""))
     }
 }
 
